fd34910882cff9e1604a0acf3222f117bafc06e9
Marcelo Laos <mlaos1997@gmail.com>
2019-02-21 20:31:20 -0500

    finished nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// JS goes here
+class NavExtended {
+    constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelector('.nav-button');
+    this.content = this.element.querySelector('.nav-content');
+    this.close = this.element.querySelector('.close');
+
+    this.button.addEventListener('click', () => this.toggleContent());
+    this.close.addEventListener('click', () => this.toggleContent());
+
+    }
+
+    toggleContent() {
+        this.content.classList.toggle('nav-hidden');
+        this.button.classList.toggle('nav-hidden');
+        this.close.classList.toggle('nav-hidden');
+    }
+}
+
+let navMenu = document.querySelectorAll('.nav');
+navMenu = Array.from(navMenu).map(nav => new NavExtended(nav));
